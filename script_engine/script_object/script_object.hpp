9392369c3a71d5bf2f6cba3c4f8ee53e5adce313
hashirama <cosies-fades-0@icloud.com>
2025-03-31 01:43:58 +0300

    Revert "remove template self_register coz it was a mistake to use it"
    This reverts commit 428c2d59d591928b9728d43c2e8fa9fca60cf9b6.

--- script_engine/script_object/script_object.hpp
+++ script_engine/script_object/script_object.hpp
@@ -28,21 +28,63 @@ public:
 		std::cout << "in\t" << _name << "\t destructor" << std::endl;
 		std::cout << "use count of " << _ngn_ptr->lua_state()
 				  << " engine : " << _ngn_ptr.use_count() << std::endl;
+		object::self_unregister();
 	}
 
 	const std::string _name{};
 
 protected:
+	template < typename T >
+	void
+	self_register( T* ptr )
+	{
+		auto is_ok = dynamic_cast< script::object* >( ptr );
+		if ( !is_ok )
+			{
+				throw std::runtime_error(
+					"Passed pointer cannot be converted to script::object pointer" );
+			}
+
+		if ( !_ngn_ptr ) { throw std::runtime_error( "Parent engine is not available" ); }
+
+		if ( _name.empty() )
+			{
+				throw std::runtime_error( "Object name cannot be empty" );
+			}
+
+
+		if ( _ngn_ptr->globals() [ _name ] != sol::lua_nil )
+			{
+				throw std::runtime_error(
+					"script::object with name '" + _name
+					+ "' is already registered in global namespace (this environment)" );
+			}
+
+		_ngn_ptr->globals() [ _name ] = ptr;
+
+		_ngn_ptr->script( "if print then print('["
+						  + std::string( _prnt_tbl ? "table" : "global" )
+						  + " registration] " + _name + "') end" );
+	}
+
 	virtual void
-	self_register() // can throw if _name already exist
+	self_register() // can throw if name already exist
 		= 0;
 
 	virtual void
 	self_unregister()
-		= 0;
+	{
+		if ( _ngn_ptr )
+			{
+				_ngn_ptr->globals() [ _name ] = sol::lua_nil;
+				_ngn_ptr->script( "if (print) then print('[unregistered]\t" + _name
+								  + "') end" );
+			}
+	}
 
 	const engine::ptr				  _ngn_ptr{};
 
+	// const sol::table				  _slf_tbl{};
 	const std::optional< sol::table > _prnt_tbl{};
 
 private:
